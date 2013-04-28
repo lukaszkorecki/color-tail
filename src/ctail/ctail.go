83ff46f442b7f348f4d7ab8da29d2a2bc64b1598
Łukasz Korecki <lukasz@coffeesounds.com>
2013-04-28 16:57:41 +0100

    lambadas and channels

--- src/ctail/ctail.go
+++ src/ctail/ctail.go
@@ -1,38 +1,45 @@
 package main
+
 import (
-  "log"
-  "os"
-  "github.com/howeyc/fsnotify"
+	"github.com/howeyc/fsnotify"
+	"log"
+	"os"
+	"path/filepath"
 )
 
-func monitor(path string, watcher *fsnotify.Watcher) {
-  go func() {
-    for {
-      select {
-      case event := <-watcher.Event:
-        log.Println("event: ", event)
-      case err := <-watcher.Error:
-        log.Println("error: ", err)
-        log.Println("closing!", path)
-        watcher.Close()
+func monitorPath(fname string, notify chan string) {
+	log.Printf("Watching %v", fname)
+	watcher, _ := fsnotify.NewWatcher()
+	notify <- "Ready!"
 
-      }
-    }
-  }()
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Event:
+				notify <- event.Name
+			case err := <-watcher.Error:
+				log.Printf("ERROR %v", err)
+				notify <- "error!"
+				watcher.Close()
+			}
+		}
+	}()
 
-  watcher.Watch(path)
+	watcher.Watch(fname)
 }
 
 func main() {
-  fname := ""
-  if len(os.Args) == 2 {
-    fname = os.Args[1]
-  }
-  log.Printf("Watching %v", fname)
-  watcher, _ := fsnotify.NewWatcher()
-  monitor(fname, watcher)
-  for {
-    log.Println("!")
-  }
+	out := make(chan string)
+
+	for i := 1; i < len(os.Args); i++ {
+    fname, _ := filepath.Abs(os.Args[i])
+    log.Printf(fname)
+    go monitorPath(fname, out)
+	}
+
+	for {
+		message := <-out
+		log.Println(message)
+	}
 
 }
