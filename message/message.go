62ce84cc764865b5a8036d6c6dc0706f4b386938
Łukasz Korecki <lukasz@coffeesounds.com>
2013-06-17 10:24:39 +0100

    hash the file once

--- message/message.go
+++ message/message.go
@@ -15,15 +15,25 @@ type Message struct {
 
 var h = md5.New()
 
+func hashId(name string) int {
+	io.WriteString(h, name)
+	return int(h.Sum(nil)[0])
+}
+
 func hashName(name string) string {
 	io.WriteString(h, name)
 	return fmt.Sprintf("%x", h.Sum(nil)[0:3])
 }
 
+
 func (m Message) Print() {
+	hn := hashName(m.Name)
+	n := technicolor.RandPaint(hn)
+	fmt.Printf("%v:\n", technicolor.RandPaint(m.Name))
+
 	for _, s := range strings.Split(m.Event, "\n") {
-		hn := hashName(m.Name)
-		n := technicolor.RandPaint(hn)
-		fmt.Printf("%v: %v\n", n, s)
+		if len(s) > 0 {
+			fmt.Printf("%v: %v\n", n, s)
+		}
 	}
 }
