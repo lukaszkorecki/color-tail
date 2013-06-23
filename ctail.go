9154bfbe9585ae344eff044ea255244962e53d93
Łukasz Korecki <lukasz@coffeesounds.com>
2013-06-23 16:41:11 +0100

    add docs here

--- ctail.go
+++ ctail.go
@@ -12,26 +12,33 @@ import (
 	"runtime"
 )
 
-var reg = registry.New()
+// keeps a track of file size
+var sizeMap = registry.New()
 
+// the meat of the programme
+// whenever an event of a file change is received we check it's previous
+// size (if we have it) and then extract the lines added and pack them
+// into a Message to print.
+// If size was not detected we print last 10 lines <- TODO FIXME that's a lie
 func fileChanged(fname string) message.Message {
 	file, err := os.Open(fname)
 	defer file.Close()
 
 	// get file size
-	stat, err2 := file.Stat()
+	stat, statErr := file.Stat()
 	size := int64(stat.Size())
 
-	if err != nil || err2 != nil {
+	if err != nil || statErr != nil {
 		return message.Message{fname, "Can't open file!"}
 	}
 
-	lastSize, isSet := reg.Get(fname)
-	offset := int64(0)
+	lastSize, isSet := sizeMap.Get(fname)
 
+	// assume we don't have size yet...
+	offset := int64(0)
 	if isSet {
-		// we know that if it's set, it's int
-		offset, _  = lastSize.(int64)
+		// Type assert here - we know that if it's set, it's int
+		offset, _ = lastSize.(int64)
 	}
 
 	// file got trimmed - or something reported wrong size
@@ -41,15 +48,18 @@ func fileChanged(fname string) message.Message {
 
 	buf := make([]byte, offset)
 
+	// read only recently appended content
 	_, readErr := file.ReadAt(buf, offset)
 	if readErr != nil && readErr != io.EOF {
 		log.Printf("!!! Reading from %v failed: %v", fname, readErr)
 	}
 
-	reg.Set(fname, int64(size))
+	// update file's size in the registry
+	sizeMap.Set(fname, int64(size))
 	return message.Message{fname, string(buf)}
 }
 
+// main... event handler so to speak
 func monitorPath(fname string, notify chan message.Message) {
 	watcher, _ := fsnotify.NewWatcher()
 	notify <- message.Message{fname, fmt.Sprintf("Start! %v", fname)}
@@ -62,6 +72,7 @@ func monitorPath(fname string, notify chan message.Message) {
 			case err := <-watcher.Error:
 				notify <- message.Message{fname, fmt.Sprintf("Error: %v", err)}
 				watcher.Close()
+				// XXX FIXME remove file from watchers collection?
 			}
 		}
 	}()
@@ -73,6 +84,7 @@ func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	out := make(chan message.Message)
+
 	if len(os.Args) == 1 {
 		log.Fatal("Needs files!")
 		os.Exit(1)
@@ -81,7 +93,8 @@ func main() {
 	for i := 1; i < len(os.Args); i++ {
 		fname, _ := filepath.Abs(os.Args[i])
 		// XXX check if file is readable?
-		reg.Set(fname, 0)
+		// read file size here and include it in sizeMap?
+		sizeMap.Set(fname, 0)
 		go monitorPath(fname, out)
 	}
 
