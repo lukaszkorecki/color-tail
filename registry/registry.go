e52be201d1ed569aa05e1688a94486b78ff6e5f3
Łukasz Korecki <lukasz@coffeesounds.com>
2013-06-23 16:42:51 +0100

    registry docs

--- registry/registry.go
+++ registry/registry.go
@@ -2,23 +2,26 @@ package registry
 
 import "sync"
 
-type AnyValue interface {}
+type AnyValue interface{}
 
-// Registry is a thread-safe map with strings as keys and any value
-// locking is required because underlying map will be read and written to
-// many go routines
+// Registry is a thread-safe map with strings as keys and anything can be
+// stored as a  value.
+// Locking is required because underlying map will be read and written to
+// by many go routines.
 type Registry struct {
 	lock  sync.RWMutex
 	store map[string]AnyValue
 }
 
-func (r *Registry) Get(key string) (AnyValue, bool){
+// Returns value read from given key and true or false if the key exists
+func (r *Registry) Get(key string) (AnyValue, bool) {
 	r.lock.RLock()
 	defer r.lock.RUnlock()
 	val, status := r.store[key]
 	return val, status
 }
 
+// Set (Safely) value under given key
 func (r *Registry) Set(key string, val AnyValue) AnyValue {
 	r.lock.Lock()
 	defer r.lock.Unlock()
