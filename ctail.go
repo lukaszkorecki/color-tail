6fcb90407a1e054f762cdf2e902af53de1977cb9
Łukasz Korecki <lukasz@coffeesounds.com>
2013-07-16 22:17:49 +0100

    path resolution cleanup

--- ctail.go
+++ ctail.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"./message"
 	"./filemonitor"
+	"./message"
 	"fmt"
 	"github.com/howeyc/fsnotify"
 	"log"
@@ -17,14 +17,6 @@ var (
 	version = "0.1.0"
 )
 
-// the meat of the programme
-// whenever an event of a file change is received we check it's previous
-// size (if we have it) and then extract the lines added and pack them
-// into a Message to print.
-func fileChanged(fname string) message.Message {
-	return filemonitor.Changed(fname)
-}
-
 // main... event handler so to speak
 func monitorPath(fname string, notify chan message.Message) {
 	watcher, _ := fsnotify.NewWatcher()
@@ -36,7 +28,7 @@ func monitorPath(fname string, notify chan message.Message) {
 		for {
 			select {
 			case event := <-watcher.Event:
-				notify <- fileChanged(event.Name)
+				notify <- filemonitor.Changed(event.Name)
 			case err := <-watcher.Error:
 				notify <- message.Message{fname, fmt.Sprintf("Error: %v", err)}
 				watcher.Close()
@@ -53,29 +45,44 @@ func main() {
 		os.Exit(1)
 	}
 
-	out := make(chan message.Message)
+	var filePaths []string
 
+	// get file list from arguments, ignoring 0th arg since it's the
+	// bin name
 	for _, argPath := range os.Args[1:] {
 		paths, err := filepath.Glob(argPath)
 		if err != nil {
-			log.Fatalf("Invalid path! %v", argPath);
+			log.Fatalf("Invalid path! %v", argPath)
 			os.Exit(1)
 		}
 
 		for _, path := range paths {
 			fname, _ := filepath.Abs(path)
-
-			if filemonitor.InitialSize(fname) {
-				go monitorPath(fname, out)
-			} else {
-				log.Printf("!! File can't be read!: %v", fname)
-				os.Exit(1)
-			}
+			filePaths = append(filePaths, fname)
 		}
 	}
 
-	for {
-		message := <-out
-		message.Print()
+	cnt := 0
+	out := make(chan message.Message)
+
+	for _, fname := range filePaths {
+		if filemonitor.InitialSize(fname) {
+			go monitorPath(fname, out)
+			cnt += 1
+		} else {
+			cnt -= 1
+			log.Printf("!! File can't be read!: %v", fname)
+		}
+	}
+
+	if cnt > 0 {
+		for {
+			message := <-out
+			message.Print()
+		}
+	} else {
+		log.Print("No files to monitor!")
+
+		os.Exit(1)
 	}
 }
